8179f349274a83547868e45b19aa5626547b187c
Adam Quinton <quintonish@gmail.com>
2014-10-09 11:11:20 -0600

    First commit of Hello World.

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * HelloWorld by Adam Quinton
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+       //Print one line to screen
+       System.out.println("Hello Github World");
+    }
+}
