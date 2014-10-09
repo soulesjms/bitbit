ea3b9a243f88c2795c2ad40a0072694ec0ccf0dc
Adam Quinton <quintonish@gmail.com>
2014-10-09 11:20:56 -0600

    Added line- Testing...

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
     public static void main(String[] args) {
        //Print one line to screen
        System.out.println("Hello Github World");
+       System.out.println("Testing...");
     }
 }
