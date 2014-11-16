7936420ef3678814d3aa2e98b245ea485f5ef842
Adam Quinton <quintonish@gmail.com>
2014-11-15 20:56:58 -0700

    Added TestNg Test for color conversion

---
+++ test/BmpTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import bitbit.BitBit;
+import static bitbit.BitBit.hexToInt;
+import static bitbit.BitBit.intToHex;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.testng.Assert;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author adam
+ */
+public class BmpTest {
+
+    public BmpTest() {
+    }
+
+    @Test
+    //test if content is set right
+    public void colorTest() {
+        /*        int[][] bitmap = null;
+        
+        String temp = null;
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+        temp = "/home/adam/Desktop/HomeMade.bmp";
+        try {
+        bitmap = new BitBit().seeBMPImage(temp);
+        } catch (IOException ex) {
+        Logger.getLogger(BmpTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        BitBit.displayArray2D(bitmap, 4, 4);
+        Assert.assertEquals(intToHex(bitmap[1][1]), intToHex(-1));*/
+        System.out.println(intToHex(-1));
+        Assert.assertEquals("ffffffff", intToHex(-1));
+
+    }
+
+}
