0a751421eb08eb49351edb2b12ac25146273d12f
soulesjms <soulesjms@gmail.com>
2014-11-20 16:23:01 -0700

    Committing Bitmap Class First commit

---
+++ src/bitbit/Bitmap.java
@@ -0,0 +1,77 @@
+package bitbit;
+
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.image.*;
+
+/**
+ *
+ * @author Jonny
+ */
+public class Bitmap {
+    String bfName;
+    boolean imageProcessed;
+    boolean windowsStyle;
+    ColorModel colorModel = null;
+    int pix[];
+
+    byte bfType[];
+    int bfSize;
+    int bfOffset;
+    int biSize;
+    int biWidth;
+    int biHeight;
+    int biPlanes;
+    int biBitCount;
+    int biCompression;
+    int biSizeImage;
+    int biXPelsPerMeter;
+    int biYPelsPerMeter;
+    int biClrUsed;
+    int biClrImportant;
+
+    public Bitmap(String name) {
+        bfName = name;
+        bfType = new byte[2];
+        imageProcessed = false;
+    }
+    
+    private void extractFileHeader(DataInputStream is) throws IOException, AWTException {
+        is.read(bfType);
+        if ( bfType[0] != 'B' || bfType[1] != 'M' )
+            throw new AWTException("Not BMP format");
+        bfSize = pullVal(is, 4);
+        is.skipBytes(4);
+        bfOffset = pullVal(is, 4);
+    }
+    
+    private int pullVal(DataInputStream is, int len) throws IOException {
+        int value = 0;
+        int temp = 0;
+
+        for ( int x = 0; x < len; x++ )
+        {
+            temp = is.readUnsignedByte();
+            value += (temp << (x * 8));
+        }
+        return value;
+    }
+    
+    public static void main( String args[] )
+    {
+        try
+        {
+            FileInputStream inFile = new FileInputStream(args[0]);
+            DataInputStream is = new DataInputStream( new BufferedInputStream(inFile) );
+            
+            BmpImage im = new BmpImage(args[0]);
+            ImageProducer img = im.extractImage(is);
+            System.out.println("Output:\n" + im);
+        }
+        catch ( Exception e )
+        {
+            System.out.println(e);
+        }
+    }
+}
